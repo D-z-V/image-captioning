354c386fbe959660141cda3397920e3849bd9126
Dev Bhandari <51533301+D-z-V@users.noreply.github.com>
2022-07-05 00:45:41 +0530

    Update model.py

--- model.py
+++ model.py
@@ -38,7 +38,6 @@ class DecoderRNN(nn.Module):
                             batch_first=True)
         
         self.fc = nn.Linear(self.hidden_size, self.vocab_size)
-#         self.hidden = (torch.zeros(1, 1, self.hidden_size), torch.zeros(1, 1, self.hidden_size))
         
     
     def forward(self, features, captions):
@@ -46,24 +45,7 @@ class DecoderRNN(nn.Module):
         caption_embed = self.word_embedding(captions[:, :-1])
         caption_embed = torch.cat((features.unsqueeze(dim=1), caption_embed),1)
         output, self.hidden = self.lstm(caption_embed)
-#         print(output.shape)
-        output = self.fc(output)
-#         print(self.embed_size, self.vocab_size)
-        return output
 
-    def sample(self, inputs, states=None, max_len=20):
-        " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        
-        """Samples captions for given image features."""
-        output = []
-        (h, c) = (torch.randn(self.num_layers, 1, self.hidden_size).to(inputs.device), torch.randn(self.num_layers, 1, self.hidden_size).to(inputs.device))
-        for i in range(max_len):
-            x, (h, c) = self.lstm(inputs, (h, c))
-            x = self.fc(x)
-            x = x.squeeze(1)
-            predict = x.argmax(dim=1)
-            if predict.item() == 1:
-                break
-            output.append(predict.item())
-            inputs = self.word_embedding(predict.unsqueeze(0))
+        output = self.fc(output)
+
         return output
